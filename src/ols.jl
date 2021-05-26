75204cab0857ae3031a38b702db44f6f3a8e716d
Coba <coba@cobac.eu>
2021-05-26 11:10:24 +0200

    Avoid allocating an unnecessary matrix in ols

--- src/ols.jl
+++ src/ols.jl
@@ -3,28 +3,18 @@
 
 Optimises the β coefficients of a `Sample` by Ordinary Least Squares.
 
-See also: `ols`
 """
 function optimβ!(sample::Sample, x, y, grammar::Grammar)
     n = size(x, 1)
-    xs = Matrix{Float64}(undef, n, length(sample.trees))
+    xs = Matrix{Float64}(undef, n, length(sample.trees) + 1)
+    xs[:, 1] = ones(size(x, 1))
     for k in eachindex(sample.trees)
-        xs[:, k] = evaltree(sample.trees[k], x, grammar)
+        xs[:, k + 1] = evaltree(sample.trees[k], x, grammar)
     end 
-    β = ols(y, xs)
+    # QR decomposition
+    β = xs \ y
     for i in eachindex(sample.β)
         sample.β[i] = β[i]
     end 
     return nothing
-end 
-
-"""
-    ols(y, x)
-
-Ordinary Least Squares via QR decomposition.
-"""
-function ols(y, x)
-    X = [ones(size(x, 1)) x]
-    β = X \ y
-    return β
 end 
