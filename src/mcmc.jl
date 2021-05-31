a373fcfeb2499de111130523d9854e81720c52d2
Coba <coba@cobac.eu>
2021-05-31 22:44:25 +0200

    return nothing on mcmc!

--- src/mcmc.jl
+++ src/mcmc.jl
@@ -163,6 +163,7 @@ function mcmc!(chain::Chain, n_steps::Int=100; verbose::Bool=false)
         verbose && println("MCMC step for tree j = ", j)
         chain.samples[i] = BayesianSR.step(chain, i - 1, j, verbose=verbose)
     end 
+    return nothing
 end 
 
 function log_likelihood(sample::Sample, grammar::Grammar, x::Matrix{Float64}, y::Vector{Float64})
